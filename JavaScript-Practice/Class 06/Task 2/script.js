77c05d20c24db5a41f30e9f308afbd973ed3b9d2
Muhammad Rayyan <mrayyanshehzad@gmail.com>
2025-10-15 23:58:17 +0500

    JS Class 06 Task 2 Update border size

--- JavaScript-Practice/Class 06/Task 2/script.js
+++ JavaScript-Practice/Class 06/Task 2/script.js
@@ -1,10 +1,10 @@
 const styleImage = () => {
     const images = document.querySelectorAll('img');
 
-    images[0].style.border = '7px solid gold ';
-    images[2].style.border = '7px dashed gold';
-    images[1].style.border = '7px dotted gold';
-    images[3].style.border = '7px double gold';
+    images[0].style.border = '3px solid gold ';
+    images[2].style.border = '3px dashed gold';
+    images[1].style.border = '3px dotted gold';
+    images[3].style.border = '3px double gold';
     images[0].style.margin = '10px';
     images[1].style.margin = '10px';
     images[2].style.margin = '10px';
